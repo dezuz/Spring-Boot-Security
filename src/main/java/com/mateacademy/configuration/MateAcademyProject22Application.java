1a79dfc678830117708a5f237902f70b34f7f2cf
dezuz <seruyhurko@gmail.com>
2019-05-25 09:24:35 +0300

    initial commit

---
+++ src/main/java/com/mateacademy/configuration/MateAcademyProject22Application.java
@@ -0,0 +1,13 @@
+package com.mateacademy.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateAcademyProject22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MateAcademyProject22Application.class, args);
+    }
+
+}

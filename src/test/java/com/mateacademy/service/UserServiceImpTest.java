1f5cf98bffec97f8ff2ffdbc39b6eb7c5c1a439b
dezuz <seruyhurko@gmail.com>
2019-05-27 16:46:53 +0300

    fixed wildcard

--- src/test/java/com/mateacademy/service/UserServiceImpTest.java
+++ src/test/java/com/mateacademy/service/UserServiceImpTest.java
@@ -6,7 +6,6 @@ import com.mateacademy.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
